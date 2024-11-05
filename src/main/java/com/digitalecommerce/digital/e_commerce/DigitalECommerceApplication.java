6632ef767402343906fa89f8597325a593933539
Madhusmita Dash <madhusmitad083@gmail.com>
2024-11-05 19:33:18 +0530

    update

---
+++ src/main/java/com/digitalecommerce/digital/e_commerce/DigitalECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.digitalecommerce.digital.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalECommerceApplication.class, args);
+	}
+
+}
